dfc988a3f525d0702374a099986a73cee98a07b6
Irmina Kaczmarek <irminakacz@gmail.com>
2017-09-27 20:28:09 +0200

    change baseURL in index.js for deploy

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-const conn = axios.create({ baseURL: 'http://localhost:8000' });
+// const conn = axios.create({ baseURL: 'http://localhost:8000' });
+const conn = axios.create({ 
+  baseURL: 'https://square-check-api.herokuapp.com'
+});
 
 ReactDOM.render(<App conn={conn} />, document.getElementById('root'));
