c11a64dfc561fecd79b77801bcce09cb66f83e1d
Irmina Kaczmarek <irminakacz@gmail.com>
2017-09-27 20:21:48 +0200

    split repository by removing backend

--- backend/square_check/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from square_check.models import Task, List
-
-admin.site.register(Task)
-admin.site.register(List)
--- backend/square_check/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SquareCheckConfig(AppConfig):
-    name = 'square_check'
